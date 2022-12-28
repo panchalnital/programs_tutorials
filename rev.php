07bad92d3d1ca5fd01739c7e1530c25991745aab
panchalnital <panchalnital5@gmail.com>
2022-12-28 18:50:59 +0530

    change for function modification

--- rev.php
+++ rev.php
@@ -17,4 +17,26 @@ $num=($num/10);
 }
 echo "<br>Reverse number of 565989 is: $revsum";  
 echo "test";
+
+$data = "Nittal Pravinbhai Panchal";
+$arr=explode(' ',$data);
+$out=[];
+for($i=0;$i<count($arr);$i++){
+  $larger=1;
+  $strrr=count_chars($arr[$i],3);
+  $leng=strlen($strrr);
+  if($leng>$larger){
+    $larger=$leng;
+     $out[$arr[$i]]=$larger;
+  }
+
+  
+  
+}
+
+
+
+
+echo "<pre>";
+print_r($out);
 ?>
