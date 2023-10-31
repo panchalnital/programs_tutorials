bb52051eeab0711181161d96e01ac768b23626d3
panchalnital <panchalnital5@gmail.com>
2023-10-31 16:53:51 +0530

    api

--- SingleResponsibilityPriciple.php
+++ SingleResponsibilityPriciple.php
@@ -23,6 +23,43 @@ s=> Single Responsibility Priciple
 "A class should have one and only one reason to change,
  meaning that a class should have only one job"
 
+Sure, I can help you with that! In PHP, the Singleton design pattern ensures that a class has only one instance and provides a global point of access to that instance. To make a Singleton class thread-safe, you can use various techniques. One commonly used method is to use the double-checked locking pattern. Here's an example of a thread-safe Singleton in PHP:
+
+Copy
+class Singleton {
+    private static $instance;
+
+    private function __construct() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            // Use double-checked locking to ensure thread safety
+            synchronized (self::class) {
+                if (self::$instance === null) {
+                    self::$instance = new self();
+                }
+            }
+        }
+        return self::$instance;
+    }
+
+    public function someBusinessLogic() {
+        // Add your methods and logic here
+    }
+}
+
+// Example usage:
+$singleton = Singleton::getInstance();
+$singleton->someBusinessLogic();
+In this example:
+
+The constructor is made private to prevent direct instantiation.
+getInstance() is the method to obtain the Singleton instance.
+The double-checked locking pattern is used to ensure thread safety. It checks if the instance is null and, if so, creates a new instance within a synchronized block.
+The someBusinessLogic() method represents the business logic of your Singleton class.
+
 
 */
 
