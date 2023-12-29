2b3233d12b79a5fe454b710dd75d8c414d04707d
panchalnital <panchalnital5@gmail.com>
2023-12-29 22:00:24 +0530

    sql query

--- Mysql/query.sql
+++ Mysql/query.sql
@@ -2,6 +2,9 @@
 update gender_table set name=case
     when name='male' then 'female' else 'male' end;
 
+update employee set gender=case
+when gender='f' then 'm' else 'f' end;
+
 /*find 2nd and 3rd highest salary*/
 
 select emp_salary from employee order by emp_salary desc limit 1,1;
@@ -34,6 +37,12 @@ where emp_status=1 group by emp_name having count(emp_name)>1 order by emp_id;
 delete e1 from employee e1, employee e2 where e1.emp_id < e2.emp_id and e1.emp_email_id=e2.emp_email_id;
 
 /* fetch duplicate recored the mysql */
+select name,email from employee GROUP by email having count(email)>1;
+/*or*/
+select name, count(email) as cnt from employee group by email having cnt>1;
+/*or*/
+select email, count(1) as cnt from employee grup by 1 having cnt>1;
+
 
 select emp_name,employee_list.emp_email_id from employee_list
 inner join (select emp_email_id from employee_list 
@@ -61,6 +70,8 @@ To add a column in a table, use the following syntax:
 */
 ALTER TABLE Customers
 ADD Email varchar(255);
+Alter table employee add column did int;
+alter table employee add column is_status ENUM('0','1') default '0';
 
 /*
 To delete a column in a table, use the following syntax (notice that some database systems don't allow deleting a column):
@@ -77,11 +88,30 @@ ALTER TABLE - MODIFY COLUMN
 ALTER TABLE Customers
 MODIFY COLUMN Email varchar(255);
 
+/**
+    change the colum name 
+    rename the column name
+*/
+ALTER TABLE employee
+change is_status gender varchar(50);
+
 /*=================================================*/
 -- Suggested testing environment: 
 -- https://www.db-fiddle.com/ with MySQL version set to 8
 
 -- Example case create statement:
+create table department(
+id int primary key,
+name varchar(50)
+);
+
+insert into department(id,name)
+values(1,'it'),
+      (2,'developer'),
+      (3,'hr'),
+      (4,'support staff');
+
+
 CREATE TABLE reservations (
     id INTEGER NOT NULL PRIMARY KEY,
     apartmentName VARCHAR(50) NOT NULL,
@@ -114,3 +144,19 @@ GROUP BY
 /* out put 
 Street Avenue	4
 Tree Avenue	3 */
+
+select apartmentName,
+	datediff(max(dateTo),min(daterom))-3 as cnt
+from reservations
+group by apartmentName;
+
+/* find the record is not null and null value in query*/
+/*not null*/
+select name 
+from employee
+where phone<>''; 
+
+/*null*/
+select name 
+from employee
+where phone=''; 
